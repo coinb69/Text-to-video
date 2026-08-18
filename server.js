5ccff831baa3b4d0e054ec5ed5ac2b1fb5ea9823
coinb69 <coinb9883@gmail.com>
2026-08-18 21:01:27 +0530

    Update fallback route to serve index.html directly

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Fallback Route: Serve index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start Server
