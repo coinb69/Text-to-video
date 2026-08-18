edaab3594dca4fca3859d539140f5bf87b1ec0bf
coinb69 <coinb9883@gmail.com>
2026-08-18 21:05:28 +0530

    Correct static assets path in server.js
    Fixed path for serving static assets in server.js.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static assets from public/root folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,)));
 
 // Fallback Route: Serve index.html
 app.get('*', (req, res) => {
